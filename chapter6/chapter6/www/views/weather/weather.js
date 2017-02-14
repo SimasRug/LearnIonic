785ec257ceb0a641ccb708d4a69297f510d3c49b
SimasRug <simonas.rugevicius@gmail.com>
2017-02-14 17:50:42 +0100

    Populated the weather view

--- chapter6/chapter6/www/views/weather/weather.js
+++ chapter6/chapter6/www/views/weather/weather.js
@@ -8,7 +8,22 @@ app.controller('WeatherController',['$scope', '$http', '$stateParams', 'Settings
  $http.get('https://api.darksky.net/forecast/a9ceab460b68862a8c4f41b9108d2090/' + $stateParams.lat + ',' + $stateParams.lng/*, {params: {units: Settings.units}} */)
   .success(function(forecast){
     $scope.forecast = forecast;
+    console.log( $scope.forecast.currently);
   });
+
+
+
+  var barHeight = document.getElementsByTagName('ion-header-bar')[0].clientHeight;
+
+  $scope.getWidth = function() {
+    return window.innerWidth + 'px';
+  };
+  $scope.getTotalHeight = function() {
+    return parseInt(parseInt($scope.getHeight())*3) + 'px';
+  };
+  $scope.getHeight = function() {
+    return parseInt(window.innerHeight - barHeight) + 'px';
+  };
 
 }]);
 
